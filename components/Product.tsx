2a78fcb0578a738137f035c8c75c66fb99214964
Pedro Fernandez <pefernandez1408@gmail.com>
2022-04-07 13:49:41 -0400

    Se creo la base de datos en un archivo json

--- components/Product.tsx
+++ components/Product.tsx
@@ -1,16 +1,24 @@
+interface Props {
+    img: string
+    name: string
+    description: string
+    price: string
+}
 
-const Product = () => (
-    <div className="mx-2 rounded-sm shadow-md hover:cursor-pointer">
+const Product = ({img, name, description, price}:Props) => (
+    <div className="mx-2 mb-3 rounded-sm shadow-md hover:cursor-pointer">
         
-        <img className="aspect-auto" src="https://images.unsplash.com/photo-1543508282-6319a3e2621f?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=715&q=80" alt="" />
+        <div className="h-4/5 shadow-sm">
+            <img className="aspect-auto" src={img} alt={name} title={name} />
+        </div>
         
         <div className="m-2">
-            <h2 className="font-bold">Zapatos</h2>
+            <h2 className="font-bold">{name}</h2>
             <p className="text-sm text-slate-400">
-                Lorem ipsum dolor, sit amet consectetur adipisicing elit. 
+                {description} 
             </p>
             <h2>
-                $79.99
+                ${price}
             </h2>
         </div>
     </div>
--- components/Stock.tsx
+++ components/Stock.tsx
@@ -1,16 +1,14 @@
 import Product from "./Product"
+import db from '../utils/db.json'
 
+const data = db.map((d) => d)
+console.log(data)
 const Stock = () => (
 <div className="grid grid-cols-3 mx-28 mt-4">
-        <Product/>
-        <Product/>
-        <Product/>
-        <Product/>
-        <Product/>
-        <Product/>
-        <Product/>
-        <Product/>
-        <Product/>
+    {data.map(({id, name, img, description, price}) => (
+        <Product key={id} name={name} img={img} description={description} price={price}/>
+    ))}
+        
     </div>
 )
 
