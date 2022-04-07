67042205c8ab506af69f6d76582356c7a8e1b493
Pedro Fernandez <pefernandez1408@gmail.com>
2022-04-07 17:44:04 -0400

    Se creo el componente 'Cart'.

---
+++ components/Cart.tsx
@@ -0,0 +1,9 @@
+import {FaShoppingCart} from 'react-icons/fa'
+
+const Cart = () => (
+   <button className='bg-slate-50 w-8 h-8 rounded-full shadow-lg shadow-black/40'>
+       <FaShoppingCart className='ml-1.5'/>
+   </button>
+)
+
+export default Cart;
--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,7 +1,11 @@
+import Cart from "./Cart"
 
 const Navbar = () => (
-    <nav className="bg-violet-500 mx-20 py-5 rounded-md mt-6">
+    <nav className="flex place-content-between bg-violet-500 mx-20 py-5 rounded-md mt-6">
         <h1 className="text-white ml-4 font-bold">My e-comerce</h1>
+        <div className="mr-10">
+        <Cart/>
+        </div>
     </nav>
 )
 
