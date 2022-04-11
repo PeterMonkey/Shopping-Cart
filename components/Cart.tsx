2fcdd0dd6e9c8369cfe4886b2bc1064203af1ee9
Pedro Fernandez <pefernandez1408@gmail.com>
2022-04-10 23:13:35 -0400

    Navegacion del Navbar

--- components/Cart.tsx
+++ components/Cart.tsx
@@ -1,9 +1,13 @@
+import React, { ReactNode } from 'react';
 import {FaShoppingCart} from 'react-icons/fa'
 
-const Cart = () => (
-   <button className='bg-slate-50 w-8 h-8 relative rounded-full shadow-lg shadow-black/40'>
-       <FaShoppingCart className='ml-1.5'/>
-   </button>
-)
-
+const Cart = React.forwardRef(({ onClick, href}:any) => {
+    return(
+        <a href={href} onClick={onClick} >
+            <button className='bg-slate-50 w-8 h-8 relative rounded-full shadow-lg shadow-black/40'>
+                <FaShoppingCart className='ml-1.5'/>
+            </button>
+        </a>
+    )
+})
 export default Cart;
--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,12 +1,19 @@
+import  Link  from "next/link"
 import Cart from "./Cart"
 import Notification from "./Notification"
 
 const Navbar = () => (
     <nav className="flex place-content-between bg-violet-500 mx-20 py-5 rounded-md mt-6">
-        <h1 className="text-white ml-4 font-bold">My e-comerce</h1>
+        <Link href="/">
+            <a>
+                <h1 className="text-white ml-4 font-bold">My e-comerce</h1>
+            </a>
+        </Link>
         <div className="mr-10">
             <Notification count={2}/>
-            <Cart/>
+            <Link href="/Cart" passHref>
+                <Cart/>
+            </Link>
         </div>
     </nav>
 )
