eaacade83252f351bf2271f3df90a49d6e5ae123
Pedro Fernandez <pefernandez1408@gmail.com>
2022-04-10 22:51:34 -0400

    Configuracion del contexto de la aplicacion

--- components/Product.tsx
+++ components/Product.tsx
@@ -1,13 +1,22 @@
-interface Props {
-    img: string
-    name: string
-    description: string
-    price: string
-    setModal: any
-}
+import {GlobalContext} from '../context/context'
+import {useContext} from 'react'
+import { ProductProps } from '../utils/interfaces'
 
-const Product = ({img, name, description, price, setModal}:Props) => (
-    <div onClick={() => setModal(true)} className="mx-2 mb-3 overflow-visible bg-white rounded-sm shadow-md hover:cursor-pointer">
+const Product = ({img, name, description, price}:ProductProps) => {
+
+    const { dataModal } = useContext(GlobalContext)
+
+    const data = {
+        img,
+        name,
+        description,
+        price
+    }
+
+    
+
+    return (
+        <div onClick={() => dataModal(data, true)} className="mx-2 mb-3 overflow-visible bg-white rounded-sm shadow-md hover:cursor-pointer">
         
         <div className="h-4/5 shadow-sm">
             <img className="aspect-auto" src={img} alt={name} title={name} />
@@ -23,6 +32,7 @@ const Product = ({img, name, description, price, setModal}:Props) => (
             </h2>
         </div>
     </div>
-)
+    )
+}
 
 export default Product
