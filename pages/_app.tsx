9f7706ff213fb4f3036c8ee91e451de68931231b
Pedro Fernandez <pefernandez1408@gmail.com>
2022-04-13 09:04:07 -0400

    Footer en Cart Page

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -4,9 +4,20 @@ import type { AppProps } from 'next/app'
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
+    <>
     <GlobalProvider>
       <Component {...pageProps} />
     </GlobalProvider>
+    <footer className="flex flex-col justify-center items-center bg-violet-600 h-32 mt-4">
+    <div className='text-lg'>
+      <h1>© Pedro Fernandez </h1>
+    </div>
+    <div className=' flex flex-col justify-center items-center mt-3'>
+      <a href="https://nextjs.org/" target="_blank">Nextjs</a>
+      <a href="https://tailwindcss.com/" target="_blank">Tailwind</a>
+    </div>
+  </footer>
+  </>
   )
 }
 
