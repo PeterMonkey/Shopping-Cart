3182ce07f73fb840f0f2b1cf15c54577d4a5e898
Pedro Fernandez <pefernandez1408@gmail.com>
2022-04-13 00:58:06 -0400

    Creacion del Footer

--- pages/index.tsx
+++ pages/index.tsx
@@ -32,18 +32,15 @@ const Home: NextPage = () => {
         }
       </main>
 
-      {/* <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <span className={styles.logo}>
-            <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
-          </span>
-        </a>
-      </footer> */}
+      <footer className="flex flex-col justify-center items-center bg-violet-600 h-32 mt-4">
+        <div className='text-lg'>
+          <h1>© Pedro Fernandez </h1>
+        </div>
+        <div className=' flex flex-col justify-center items-center mt-3'>
+          <a href="https://nextjs.org/" target="_blank">Nextjs</a>
+          <a href="https://tailwindcss.com/" target="_blank">Tailwind</a>
+        </div>
+      </footer>
     </div>
   )
 }
